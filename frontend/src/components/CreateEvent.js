954e3102896284e5356dae61bd2bfa4f3f60b2cb
k1s3n <christoffer@rehnmark.se>
2024-06-19 19:45:56 +0200

    Auto Adress googlemaps

--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -2,8 +2,7 @@ import React, { useState, useCallback } from 'react';
 import API from '../api';
 import { useAuth } from '../AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
+import { GoogleMap, Marker, Autocomplete } from '@react-google-maps/api';
 const CreateEvent = () => {
   const { token, userId } = useAuth();
   const [formData, setFormData] = useState({
@@ -15,7 +14,8 @@ const CreateEvent = () => {
     date: '',
     time: '',
   });
-  const [mapClicked, setMapClicked] = useState(false);
+  const [mapCenter, setMapCenter] = useState({ lat: 51.1657, lng: 10.4515 }); // Initial center of the map
+  const [autocomplete, setAutocomplete] = useState(null);
   const [markerPosition, setMarkerPosition] = useState({ lat: 59.3293, lng: 18.0686 }); // Default to Stockholm coordinates
 
   const handleChange = (e) => {
@@ -31,24 +31,70 @@ const CreateEvent = () => {
       latitude,
       longitude,
     }));
-    setMapClicked(true);
+    setMapCenter({ lat: latitude, lng: longitude });
+    fetchAddressFromCoordinates(latitude, longitude);
   }, []);
+
+  const handleAutocompleteLoad = (autocompleteInstance) => {
+    setAutocomplete(autocompleteInstance);
+  };
+
+  const handlePlaceChanged = () => {
+    if (autocomplete !== null) {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        const location = place.geometry.location;
+        setMapCenter({ lat: location.lat(), lng: location.lng() });
+        setMarkerPosition({ lat: location.lat(), lng: location.lng() });
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          location: place.formatted_address,
+          latitude: location.lat(),
+          longitude: location.lng(),
+        }));
+      } else {
+        console.error('Place has no geometry');
+      }
+    } else {
+      console.error('Autocomplete is not loaded');
+    }
+  };
+
+  const fetchAddressFromCoordinates = (latitude, longitude) => {
+    const geocoder = new window.google.maps.Geocoder();
+    const latlng = { lat: latitude, lng: longitude };
+
+    geocoder.geocode({ location: latlng }, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          const address = results[0].formatted_address;
+          setFormData((prevFormData) => ({
+            ...prevFormData,
+            location: address,
+          }));
+        } else {
+          console.error('No results found');
+        }
+      } else {
+        console.error('Geocoder failed due to: ' + status);
+      }
+    });
+  };
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await API.post(
+      await API.post(
         '/createEvent',
         { ...formData, createdBy: userId },
         {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-      console.log(response.data);
-      console.log('Event created successfully', formData);
       navigate('/'); // Handle success here, e.g., reset the form or show a message
+      
     } catch (error) {
       console.error('Error creating event:', error);
     }
@@ -56,57 +102,63 @@ const CreateEvent = () => {
 
   return (
     <div>
-      <h3>Create Event</h3>
-      <div style={{ height: '400px', width: '100%' }}>
-          <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <h3>Create Event</h3>
+        <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+          <strong>Selected Address:</strong> {formData.location}
+        </div>
+        <div style={{ display: 'flex', gap: '20px' }}>
+          <div style={{ height: '400px', width: '50%' }}>
             <GoogleMap
-              mapContainerStyle={{ height: '100%', width: '50%' }}
-              center={markerPosition}
-              zoom={10}
+              mapContainerStyle={{ height: '100%', width: '100%' }}
+              center={mapCenter}
+              zoom={15}
               onClick={handleMapClick}
             >
-              {mapClicked && (
-                <Marker position={markerPosition} />
-              )}
+              <Marker position={markerPosition} />
             </GoogleMap>
-          </LoadScript>
+          </div>
+          <form onSubmit={handleSubmit} style={{ width: '50%' }}>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={handleChange}
+              required
+            />
+            <br></br>
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              onChange={handleChange}
+              required
+            />
+            <br></br>
+            <Autocomplete onLoad={handleAutocompleteLoad} onPlaceChanged={handlePlaceChanged}>
+            <input
+            type="text"
+            placeholder="Enter a location"
+            value={formData.location}
+            onChange={handleChange}
+            name="location"
+            />
+            </Autocomplete>
+            <input
+              type="date"
+              name="date"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="time"
+              name="time"
+              onChange={handleChange}
+              required
+            />
+            <br></br>
+            <button type="submit">Create Event</button>
+          </form>
         </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="location"
-          placeholder="Location"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="date"
-          name="date"
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="time"
-          name="time"
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Create Event</button>
-      </form>
     </div>
   );
 };
